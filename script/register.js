a0e4ebf598533d36eba5a2e3fd17cd40d048074c
kenglee123456789 <148339885+kenglee123456789@users.noreply.github.com>
2025-04-22 16:33:31 +0700

    Update register.js

--- script/register.js
+++ script/register.js
@@ -212,7 +212,7 @@ function insertRegisdetail() {
     const regisdetail = {
       regisID: document.querySelector('input[name="regisID"]').value,
       subject: subjectID,
-      scholarship: "2",
+      scholarship: "1",
       Cost: subjectCost,
     };
 
